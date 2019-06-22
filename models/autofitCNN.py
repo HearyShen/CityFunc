4755ce1fafd4846be1cf1c1749324dcf7888299e
Jiayun Shen <shenjiayun1996@126.com>
2019-06-23 00:37:44 +0800

    initial VisitFitNet

--- models/autofitCNN.py
+++ models/autofitCNN.py
@@ -3,10 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
 class ImageFitNet(nn.Module):
     def __init__(self):
         super(ImageFitNet, self).__init__()
-        self.model = models.resnet34(num_classes=9)
+        self.model = models.resnet34(num_classes=256)
 
     def forward(self, x):
         out = self.model(x)
@@ -15,10 +21,67 @@ class ImageFitNet(nn.Module):
 class VisitFitNet(nn.Module):
     def __init__(self):
         super(VisitFitNet, self).__init__()
-        pass
+        self.conv1 = nn.Conv2d(24, 64, 3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        
+        self.conv5 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(256)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc = nn.Linear(256, 256)
 
     def forward(self, x):
-        return None
+        identity = x            # x: batch_size×24×7×26
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += identity
+        out = self.relu(out)
+        out = self.maxpool(out) # out: batch_size×64×4×14
+
+        identity = out
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv4(out)
+        out = self.bn4(out)
+        out += identity
+        out = self.relu(out)
+        out = self.maxpool(out) # out: batch_size×128×3×8
+
+        identity = out
+        out = self.conv5(out)
+        out = self.bn5(out)
+        out = self.relu(out)
+        out = self.conv6(out)
+        out = self.bn6(out)
+        out += identity
+        out = self.relu(out)
+        out = self.maxpool(out) # out: batch_size×256×2×5
+
+        out = self.avgpool(out) # out: batch_size×256×1×1
+
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)      # out: batch_size×256
+
+        return out
 
 
 class AutoFitNet(nn.Module):
