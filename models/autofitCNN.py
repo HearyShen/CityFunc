6e89d0d3eef2aa98a91423dd175ff83745916ecf
HearyShen <jiayun.shen@foxmail.com>
2019-06-24 14:12:39 +0800

    resne34t+visitDP averaged with attention

--- models/autofitCNN.py
+++ models/autofitCNN.py
@@ -20,9 +20,10 @@ class AutoFitNet(nn.Module):
         super(AutoFitNet, self).__init__()
         # init models
         self.image_model = ImageFitNet()
-        self.visit_model_24x7x26 = VisitFitNet(24, 128)
-        self.visit_model_26x7x24 = VisitFitNet(26, 128)
-        self.fc = nn.Linear(512, 9) 
+        self.visit_model_24x7x26 = VisitFitNet(24, 256)
+        self.visit_model_26x7x24 = VisitFitNet(26, 256)
+        self.context = torch.rand(256).cuda()
+        self.fc = nn.Linear(256, 9) 
 
     def forward(self, x):
         # image: batch_size×3×224×224
@@ -31,9 +32,14 @@ class AutoFitNet(nn.Module):
         visit_24x7x26 = visit
         visit_26x7x24 = visit.permute(0, 3, 2, 1)
         image_out = self.image_model(image)             # image_out: batch_size×256
-        visit_out_weekdayAndWeek = self.visit_model_24x7x26(visit_24x7x26)     # visit_out_weekdayAndWeek: batch_size×128
-        visit_out_weekdayAndHour = self.visit_model_26x7x24(visit_26x7x24)     # visit_out_weekdayAndHour: batch_size×128
+        visit_out_weekdayAndWeek = self.visit_model_24x7x26(visit_24x7x26)     # visit_out_weekdayAndWeek: batch_size×256
+        visit_out_weekdayAndHour = self.visit_model_26x7x24(visit_26x7x24)     # visit_out_weekdayAndHour: batch_size×256
+
         out = torch.cat((image_out, visit_out_weekdayAndWeek, visit_out_weekdayAndHour), 1)
+        out = torch.reshape(out, (-1, 3, 256))          # out: batch_size×3×256
+        attention = F.softmax(torch.matmul(out, self.context), dim=1).reshape(-1,3,1)   # attention: batch_size×3×1
+        out = torch.mul(out, attention)                 # out: batch_size×3×256
+        out = torch.sum(out, dim=1)                     # out: batch_size×256
         out = self.fc(out)
 
         return F.softmax(out, dim=1)
